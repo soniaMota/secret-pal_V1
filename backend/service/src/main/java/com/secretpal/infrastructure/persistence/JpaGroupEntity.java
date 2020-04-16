c742f1cc32ab7cf0965dd323ad718febd2938355
soniaMota <s.mota.diaz@iescristobaldemonroy.es>
2020-04-16 10:20:47 +0200

    add query fingByName modify query findAllGroups for findAllNameGroups

--- backend/service/src/main/java/com/secretpal/infrastructure/persistence/JpaGroupEntity.java
+++ backend/service/src/main/java/com/secretpal/infrastructure/persistence/JpaGroupEntity.java
@@ -1,11 +1,19 @@
 package com.secretpal.infrastructure.persistence;
 
+import com.secretpal.domain.model.Event;
+import com.secretpal.domain.model.User;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -13,8 +21,10 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "group", schema = "public")
+
 @NamedQueries({
-        @NamedQuery(name = "findAllGroups", query = "select g from JpaGroupEntity g")})
+        @NamedQuery(name = "findAllNameGroups", query = "select g.name from JpaGroupEntity g"),
+        @NamedQuery(name = "findByName", query = "select g from JpaGroupEntity g where g.name = :name")})
 public class JpaGroupEntity {
 
     @Id
@@ -24,5 +34,15 @@ public class JpaGroupEntity {
 
     @Column(name = "name")
     private String name;
+
+    //@ManyToOne
+    //JoinColumn(name = "id")
+    private User userAdmin;
+
+    @ManyToMany(mappedBy = "groups")
+    private List<User> users;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL)
+    private List<Event> events;
 
 }
